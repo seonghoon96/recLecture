05c352af2c31114caa20052dc2e8b394c41e8019
seunghoon96 <psh104404@naver.com>
2021-08-17 00:32:49 +0900

    commit

--- RecommendLectures/urls.py
+++ RecommendLectures/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
-
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('rec/',views.index,name = 'FIRSTPAGE'),
-    path('logincheck/',views.logincheck,name = 'logincheck'),
-    url(r'^logincheck/RecommendLectures/$',views.RecommendLecture,name = 'RecommendLecture')
+    path('admin/', admin.site.urls),
+    path('rec/', views.index, name='FIRSTPAGE'),
+    path('logincheck/', views.logincheck, name='logincheck'),
+    url(r'^logincheck/RecommendLectures/$', views.RecommendLecture, name='RecommendLecture')
 ]
